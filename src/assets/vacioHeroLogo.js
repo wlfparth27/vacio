63b248986d3f9f951667f7bda4adf98ad28acee6
Parth <wlfparth27@gmail.com>
2026-09-16 14:45:32 +0530

    Remove temporary hero asset

--- src/assets/vacioHeroLogo.js
+++
@@ -1,3 +0,0 @@
-const vacioHeroLogo = `data:image/svg+xml;charset=UTF-8,${encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1697 927"><rect width="1697" height="927" fill="#050505"/><text x="848.5" y="535" text-anchor="middle" font-family="Georgia, 'Times New Roman', serif" font-size="420" letter-spacing="-35" fill="#fff">VACIO.</text></svg>`)}`
-
-export default vacioHeroLogo
